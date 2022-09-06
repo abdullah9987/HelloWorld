af7c75dfdebd0f7c5ac76d1ec0289f13fa7927dd
abdullah9987 <112861916+abdullah9987@users.noreply.github.com>
2022-09-06 22:36:21 +0500

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Arrays {
+    
+    public static void main(String args []) {
+        int [] price = new int [4];
+        price[0] = 224; // $
+        price[1] = 100; // dairy milk
+        price[2] = 245; //petrol
+        price[3] = 100; //fries
+      //  System.out.println(price[0]);
+      //  System.out.println(price[1]);
+       // System.out.println(price[2]);
+      //  System.out.println(price[3]);
+
+      for(int i = 0; i < 4; i++) {
+        System.out.println(price[i]);
+      }
+    }
+}
